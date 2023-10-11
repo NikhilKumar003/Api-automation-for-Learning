49fa41af021f36341ebd918cc7620e0ed5d1ad8b
nikhil003 <nikhil.enabothulaa@gmail.com>
2023-10-11 23:48:15 +0530

    post api request java

---
+++ Postapirequest.java
@@ -0,0 +1,44 @@
+package com.testautomation.apitesting.tests;
+
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import net.minidev.json.JSONObject;
+
+public class Postapirequest {
+	@Test
+	public void createbooking()
+	{
+//		prepare request body
+		JSONObject booking = new JSONObject();
+		JSONObject  bookingdate = new JSONObject();
+		
+		booking.put("firstname", "apiautomation");
+		booking.put("lastname", "practice");
+		booking.put("totalprice", 3500);
+		booking.put("depositpaid", true);
+		booking.put("additionalneeds", "Breakfast");
+		booking.put("bookingdates", bookingdate);
+		
+		bookingdate.put("checkin", "2021-03-01");
+		bookingdate.put("checkout", "2022-03-09");
+		
+		RestAssured
+		.given()
+		.contentType(ContentType.JSON)
+		.body(booking.toString())
+		.baseUri("https://restful-booker.herokuapp.com/booking")
+		.log().all()
+		.when()
+		.post()
+		.then()
+		.assertThat()
+		.statusCode(200)
+		.body("booking.firstname",Matchers.equalTo("apiautomation"))
+		.body("booking.totalprice",Matchers.equalTo(3500))
+		.body("booking.bookingdates.checkin",Matchers.equalTo("2021-03-01"));
+	}
+
+}
